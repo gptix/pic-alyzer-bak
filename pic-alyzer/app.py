6a61aecd3f0951ca82dadad70d6c72c6c31e8851
Jud Taylor <jud.taylor@protonmail.com>
2020-02-04 15:33:09 -0500

    Added simple route..

--- pic-alyzer/app.py
+++ pic-alyzer/app.py
@@ -12,26 +12,26 @@ def create_app():
 
 
     @app.route('/summary', methods=['POST'])
-        """Receive and process a request for classification of one image."""
+    """Receive and process a request for classification of one image."""
     def summary():
         return "{'frogs' : '2'}"
 
 
     # @app.route('/batch_img_summary', methods=['POST'])
-    # """Receive and process a request for classification of a batch of images."""
-    # ### Sample message :
-    # # {
-	# #"images": [
-	# #	"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1",
-	# #	"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/b46c5e2c17813f956df74118d60cfff7",
-	# #]
-    # #}
-    # def batch_img_summary():   
-    #     return "{'classifications' : {{'frogs' : '4'}, {'sharks' : '17'}}}"
+        # """Receive and process a request for classification of a batch of images."""
+        # ### Sample message :
+        # # {
+	    # #"images": [
+	    # #	"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1",
+	    # #	"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/b46c5e2c17813f956df74118d60cfff7",
+	    # #]
+        # #}
+        # def batch_img_summary():   
+        #     return "{'classifications' : {{'frogs' : '4'}, {'sharks' : '17'}}}"
     
     return app
 
-       # Sample message containing URL for one image:
+    # Sample message containing URL for one image:
     #{
 	#"image": "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1"
     #}
