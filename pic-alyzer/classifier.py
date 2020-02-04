debfde936122b376bafd811bb30145027648b8b9
Jud Taylor <jud.taylor@protonmail.com>
2020-02-04 17:14:26 -0500

    Made result of classifier depend on path passed.

--- pic-alyzer/classifier.py
+++ pic-alyzer/classifier.py
@@ -1,6 +1,6 @@
 def img_pred(path):
    """Sends a path to an image to the classifier, should return a JSON object."""
-   if path = "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1":
+   if path == "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1":
       result =  "{'frogs' : '3'}"
    else:
       result = "{'sharks' : '17'}"
