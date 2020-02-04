a09d199b2641c6971ba3b0121d40dffb58735490
Jud Taylor <jud.taylor@protonmail.com>
2020-02-04 15:26:48 -0500

    Cut back to just index.

--- pic-alyzer/app.py
+++ pic-alyzer/app.py
@@ -1,6 +1,5 @@
 """Simplest Heroku app that receives requests and provides responses per picfest project."""
 from flask import Flask, request
-import gunicorn
 
 def create_app():
     """Create and configure an instance of the Flask application"""
@@ -12,30 +11,28 @@ def create_app():
         return "<html><body><h1>Hi there!</h1></body></html>"
 
 
-    @app.route('/summary', methods=['POST'])
-    """Receive and process a request for classification of one image."""
-    # Sample message containing URL for one image:
-    #{
-	#"image": "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1"
-    #}
+    # @app.route('/summary', methods=['POST'])
+    #     """Receive and process a request for classification of one image."""
+    # # Sample message containing URL for one image:
+    # #{
+	# #"image": "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1"
+    # #}
 
-    def summary():
-        message = "{'frogs' : '2'}"
-        return message
+    # def summary():
+    #     message = "{'frogs' : '2'}"
+    #     return message
 
 
-    @app.route('/batch_img_summary', methods=['POST'])
-    """Receive and process a request for classification of a batch of images."""
-    ### Sample message :
-    # {
-	#"images": [
-	#	"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1",
-	#	"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/b46c5e2c17813f956df74118d60cfff7",
-	#]
-    #}
-
-    def batch_img_summary():   
-        message = "{'classifications' : {{'frogs' : '4'}, {'sharks' : '17'}}}"
-        return message
-
+    # @app.route('/batch_img_summary', methods=['POST'])
+    # """Receive and process a request for classification of a batch of images."""
+    # ### Sample message :
+    # # {
+	# #"images": [
+	# #	"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1",
+	# #	"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/b46c5e2c17813f956df74118d60cfff7",
+	# #]
+    # #}
+    # def batch_img_summary():   
+    #     return "{'classifications' : {{'frogs' : '4'}, {'sharks' : '17'}}}"
+    
     return app
