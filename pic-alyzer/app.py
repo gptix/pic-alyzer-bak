7b4fcfdec104e7bf55be7e9701293ec801014b0a
Jud Taylor <jud.taylor@protonmail.com>
2020-02-04 15:19:41 -0500

    Removed extra return.

--- pic-alyzer/app.py
+++ pic-alyzer/app.py
@@ -11,36 +11,31 @@ def create_app():
     def index():
         return "<html><body><h1>Hi there!</h1></body></html>"
 
+
     @app.route('/summary', methods=['POST'])
-    """Receive and process a request for classification of one image. Sample message containing URL for one image:
+    """Receive and process a request for classification of one image."""
+    # Sample message containing URL for one image:
+    #{
+	#"image": "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1"
+    #}
 
-{
-	"image": "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1"
-}
-"""
     def summary():
-    
         message = "{'frogs' : '2'}"
-
         return message
-
-        return app
 
 
     @app.route('/batch_img_summary', methods=['POST'])
-"""Receive and process a request for classification of a batch of images. Sample message :
+    """Receive and process a request for classification of a batch of images."""
+    ### Sample message :
+    # {
+	#"images": [
+	#	"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1",
+	#	"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/b46c5e2c17813f956df74118d60cfff7",
+	#]
+    #}
 
-{
-	"images": [
-		"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1",
-		"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/b46c5e2c17813f956df74118d60cfff7",
-	]
-}
-"""
-    def batch_img_summary():
-    
+    def batch_img_summary():   
         message = "{'classifications' : {{'frogs' : '4'}, {'sharks' : '17'}}}"
-    
         return message
 
     return app
