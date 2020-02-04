dafa65456226e9dd798fcdc356ea34c229fc3d05
Jud Taylor <jud.taylor@protonmail.com>
2020-02-04 18:00:39 -0500

    think I got join working.

--- pic-alyzer/classifier.py
+++ pic-alyzer/classifier.py
@@ -3,6 +3,6 @@ def img_pred(path):
    if path == "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1":
       result =  "{'frogs' : '3'}"
    else:
-      result = "{'sharks' : '17'}"
+      result = {'sharks' : '17'}
 
    return result
