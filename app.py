78db4b610d9356f4afccc40a7e0f2a43439f391f
Jud Taylor <jud.taylor@protonmail.com>
2020-02-04 14:26:36 -0500

    added code to app.py

--- app.py
+++ app.py
@@ -1,1 +1,39 @@
 """Simplest Heroku app that receives requests and provides responses per picfest project."""
+from flask import Flask, request
+
+def create_app():
+    """Create and configure an instance of the Flask application"""
+    app = Flask(__name__)
+    DB.init_app(app)
+
+@app.route('/summary', methods=['POST'])
+"""Receive and process a request for classification of one image. Sample message containing URL for one image:
+
+{
+	"image": "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1"
+}
+"""
+def summary():
+    
+    message = "{'frogs' : '2'}"
+
+    return message
+
+    return app
+
+
+@app.route('/batch_img_summary', methods=['POST'])
+"""Receive and process a request for classification of a batch of images. Sample message :
+
+{
+	"images": [
+		"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1",
+		"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/b46c5e2c17813f956df74118d60cfff7",
+	]
+}
+"""
+def batch_img_summary():
+    
+    message = "{'classifications' : {{'frogs' : '4'}, {'sharks' : '17'}}}"
+    
+    return message
