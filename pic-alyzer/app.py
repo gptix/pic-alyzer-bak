134db67f708ad1e10bd1a9672d82cddb7828aca2
Jud Taylor <jud.taylor@protonmail.com>
2020-02-05 12:08:02 -0500

    PEP8'd app.py.  One unavoidable error (length of long value).

--- pic-alyzer/app.py
+++ pic-alyzer/app.py
@@ -1,29 +1,29 @@
-"""Simplest Heroku app that receives requests and provides responses per picfest project."""
+"""Simplest Heroku app that receives requests and provides responses
+per picfest project."""
 import json
 from flask import Flask, request
 from .classifier import img_pred
+
 
 def create_app():
     """Create and configure an instance of the Flask application"""
     app = Flask(__name__)
 
-
     @app.route('/')
     def index():
         return "<html><body><h1>Hi there!</h1></body></html>"
-
 
     @app.route('/summary', methods=['GET', 'POST'])
     def summary():
         """Receive and process a request for classification of one image."""
         # image_path=request['image']
-        image_path = "foo" # dummy function doesn't even use value.
+        image_path = "foo"  # dummy function doesn't even use value.
         return img_pred(image_path)
-        
 
     @app.route('/batch_img_summary', methods=['GET', 'POST'])
-    def batch_img_summary():   
-        """Receive and process a request for classification of a batch of images."""
+    def batch_img_summary():
+        """Receive and process a request for classification of a batch of
+        images."""
         # image_paths = request['images']
         image_paths = ['foo', 'foo']
 
@@ -33,20 +33,17 @@ def create_app():
             results.append(result)
         return "{'classifications' : " + json.dumps(results) + "}"
 
-
     return app
 
+# Sample message containing URL for one image:
+# {
+# "image": "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1"
+# }
 
-    # Sample message containing URL for one image:
-    #{
-	#"image": "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1"
-    #}
-
-    ### Sample message :
-        # {
-	    #"images": [
-	    #	"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1",
-	    #	"https://michelangelostestbucket.s3.us-west-2.amazonaws.com/b46c5e2c17813f956df74118d60cfff7",
-	    #]
-        #}
-    
+# Sample message :
+# {
+# "images": [
+# "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1",
+# "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/b46c5e2c17813f956df74118d60cfff7",
+# ]
+# }
