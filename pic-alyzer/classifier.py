b89e52909f83f598bf0d0b4805ebc9a1e23eb88e
Jud Taylor <jud.taylor@protonmail.com>
2020-02-05 12:12:19 -0500

    PEP8'd classifier.py.  One unavoidable error (length of long value).

--- pic-alyzer/classifier.py
+++ pic-alyzer/classifier.py
@@ -1,8 +1,9 @@
 def img_pred(path):
-   """Sends a path to an image to the classifier, should return a JSON object."""
-   if path == "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1":
-      result =  "{'frogs' : '3'}"
-   else:
-      result = {'sharks' : '17'}
+    """Sends a path to an image to the classifier, should return a
+    JSON object."""
+    if path == "https://michelangelostestbucket.s3.us-west-2.amazonaws.com/8bf0322348cc11e7e3cc98325fbfcaa1":
+        result = "{'frogs' : '3'}"
+    else:
+        result = {'sharks': '17'}
 
-   return result
+    return result
